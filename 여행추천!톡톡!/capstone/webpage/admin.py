f27831dda5cde213e750261014154ca2d3d113eb
kmh_project <rkdalsghek@naver.com>
2018-11-14 06:07:39 +0900

    complete

--- 여행추천!톡톡!/capstone/webpage/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import acc_search
-
-admin.site.register(acc_search)
-
-
-# Register your models here.
--- 여행추천!톡톡!/capstone/webpage/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class acc_search(models.Model):
-    search_id = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    search_link = models.TextField()
-
-# Create your models here.
